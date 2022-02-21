efb55baeefc43bd3ea460c61150c65bad31f2434
dilumharshana <dilum.harshana123@gmail.com>
2022-02-21 10:18:41 +0530

    tring to add loading button to app

--- application-form/src/components/input-components/ButtonComponent.tsx
+++ application-form/src/components/input-components/ButtonComponent.tsx
@@ -1,2 +1,10 @@
-import * as React from 'react';
-
+import Button from "@mui/material/Button";
+import LoadingButton from "@mui/lab/LoadingButton";
+export const ButtonComponent = () => {
+  return (
+    <div>
+      <Button>hello</Button>
+      <LoadingButton loading>loadin</LoadingButton>
+    </div>
+  );
+};
